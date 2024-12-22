555bcaecce845fa165cb8cf169cf24ce3bdf0c8a
mrskg0438 <gaudshwetkamal0438@gmail.com>
2024-12-22 13:34:27 +0530

    full code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from "react-redux";
+import store from "@/redux/store";
+import { useEffect } from "react";
+import Layout from "@/components/Layout";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+
+    if (typeof document !== 'undefined') {
+      //@ts-ignore
+      import('bootstrap/dist/js/bootstrap.min.js');
+     
+    }
+  }, []);
+  return (
+
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>)
+
 }
